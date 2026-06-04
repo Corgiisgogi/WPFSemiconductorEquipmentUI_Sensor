9a4e89478090798ca291459ede40ae2ef05b7cbf
Corgi <User@V308-11>
2026-06-04 09:02:28 +0900

    Distance > Vibration으로 수정

--- WPFSemiconductorEquipmentUI_Sensor/ViewModels/ConsoleViewModel.cs
+++ WPFSemiconductorEquipmentUI_Sensor/ViewModels/ConsoleViewModel.cs
@@ -13,7 +13,7 @@ namespace WPFSemiconductorEquipmentUI_Sensor.ViewModels
             Sensors = new ObservableCollection<SensorMetric>
             {
                 new SensorMetric { Name = "Pressure", Value = "2.4", Unit = "bar", RangeText = "Normal 1.5 - 3.0 bar", BadgeText = "NORMAL", Tone = "Normal", IndicatorWidth = 145 },
-                new SensorMetric { Name = "Distance", Value = "18", Unit = "cm", RangeText = "Normal 12 - 24 cm", BadgeText = "NORMAL", Tone = "Normal", IndicatorWidth = 145 },
+                new SensorMetric { Name = "Vibration", Value = "0.12", Unit = "g", RangeText = "Normal under 0.20 g", BadgeText = "NORMAL", Tone = "Normal", IndicatorWidth = 118 },
                 new SensorMetric { Name = "Temperature", Value = "38", Unit = "C", RangeText = "Watch over 40 C", BadgeText = "WATCH", Tone = "Warning", IndicatorWidth = 176 },
                 new SensorMetric { Name = "Humidity", Value = "41", Unit = "%", RangeText = "Normal 30 - 60 %", BadgeText = "NORMAL", Tone = "Normal", IndicatorWidth = 145 }
             };
